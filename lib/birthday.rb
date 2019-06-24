dcee924fbdb967780488b4b177411a8ab1b9b100
Lantz Warrick <betalantz@gmail.com>
2019-06-24 14:07:55 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,11 @@
 # }
 
 def happy_birthday(birthday_kids)
-  
+  birthday_kids.each {|name, age|
+    if age < 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  }
 end
